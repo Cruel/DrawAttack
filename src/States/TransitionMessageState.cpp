9e8d6e8fd84da03b52b6ae4b76b82228c495f44d
Cruel <machin3@gmail.com>
2015-11-04 11:14:32 -0500

    Add TransitionMessage state

---
+++ src/States/TransitionMessageState.cpp
@@ -0,0 +1,55 @@
+#include "TransitionMessageState.hpp"
+#include <cpp3ds/Window/Window.hpp>
+
+
+namespace DrawAttack {
+
+TransitionMessageState::TransitionMessageState(StateStack& stack, Context& context)
+: State(stack, context)
+{
+	m_text.setColor(cpp3ds::Color::White);
+	m_text.setCharacterSize(20);
+	m_text.setString(_("%s\nPress any key to continue.", getContext().transition.message.c_str()));
+
+	m_rectangle.setSize(cpp3ds::Vector2f(400, 240));
+	m_rectangle.setFillColor(cpp3ds::Color(0, 0, 0, 200));
+}
+
+void TransitionMessageState::renderTopScreen(cpp3ds::Window& window)
+{
+	window.draw(m_rectangle);
+	window.draw(m_text);
+}
+
+void TransitionMessageState::renderBottomScreen(cpp3ds::Window& window)
+{
+	window.draw(m_rectangle);
+}
+
+bool TransitionMessageState::update(float delta)
+{
+	return true;
+}
+
+bool TransitionMessageState::processEvent(const cpp3ds::Event& event)
+{
+	// If any key is pressed, trigger the next screen
+	if (event.type == cpp3ds::Event::KeyPressed)
+	{
+		requestStackPop();
+
+		States::ID newState = getContext().transition.nextStateID;
+		if (newState != States::None) {
+			requestStackPop();
+			requestStackPush(newState);
+		}
+
+		getContext().transition.message.clear();
+		getContext().transition.nextStateID = States::None;
+		return false;
+	}
+
+	return true;
+}
+
+} // namespace DrawAttack
--- src/States/TransitionMessageState.hpp
+++ src/States/TransitionMessageState.hpp
@@ -1,16 +1,17 @@
-#ifndef DRAWATTACK_DISCONNECTEDSTATE_HPP
-#define DRAWATTACK_DISCONNECTEDSTATE_HPP
+#ifndef DRAWATTACK_TRANSITIONMESSAGESTATE_HPP
+#define DRAWATTACK_TRANSITIONMESSAGESTATE_HPP
 
 #include "State.hpp"
 #include <cpp3ds/Graphics/Sprite.hpp>
 #include <cpp3ds/Graphics/Text.hpp>
+#include <cpp3ds/Graphics/RectangleShape.hpp>
 
 namespace DrawAttack {
 
-class DisconnectedState : public State
+class TransitionMessageState : public State
 {
 public:
-	DisconnectedState(StateStack& stack, Context& context);
+	TransitionMessageState(StateStack& stack, Context& context);
 
 	virtual void renderTopScreen(cpp3ds::Window& window);
 	virtual void renderBottomScreen(cpp3ds::Window& window);
@@ -19,8 +20,9 @@ public:
 
 private:
 	cpp3ds::Text m_text;
+	cpp3ds::RectangleShape m_rectangle;
 };
 
 } // namespace DrawAttack
 
-#endif // DRAWATTACK_DISCONNECTEDSTATE_HPP
+#endif // DRAWATTACK_TRANSITIONMESSAGESTATE_HPP
