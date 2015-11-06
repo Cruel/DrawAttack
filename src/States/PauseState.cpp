bd8c8abd97632fb6e3fa0b6aa671a9c254b2195e
Cruel <machin3@gmail.com>
2015-11-06 11:25:16 -0500

    Implement minimal pause menu

--- src/States/PauseState.cpp
+++ src/States/PauseState.cpp
@@ -7,19 +7,50 @@ namespace DrawAttack {
 PauseState::PauseState(StateStack& stack, Context& context)
 : State(stack, context)
 {
-	m_text.setColor(cpp3ds::Color::Black);
-	m_text.setCharacterSize(20);
-	m_text.setString("Paused.");
+	m_overlay.setSize(cpp3ds::Vector2f(400.f, 240.f));
+	m_overlay.setFillColor(cpp3ds::Color(0, 0, 0, 220));
+
+	m_texture.loadFromFile("images/button.9.png");
+
+	m_buttonResume.setTexture(&m_texture);
+	m_buttonResume.setString(_("Resume"));
+	m_buttonResume.setActiveColor(cpp3ds::Color(255, 255, 255, 180));
+	m_buttonDisconnect.setTexture(&m_texture);
+	m_buttonDisconnect.setString(_("Disconnect"));
+	m_buttonDisconnect.setActiveColor(cpp3ds::Color(255, 255, 255, 180));
+	m_buttonQuit.setTexture(&m_texture);
+	m_buttonQuit.setString(_("Quit"));
+	m_buttonQuit.setActiveColor(cpp3ds::Color(255, 255, 255, 180));
+
+	m_buttonResume.setPosition(40.f, 40.f);
+	m_buttonDisconnect.setPosition(40.f, 80.f);
+	m_buttonQuit.setPosition(40.f, 120.f);
+
+	m_buttonResume.onClick([this]{
+		requestStackPop();
+	});
+	m_buttonDisconnect.onClick([this]{
+		requestStackClear();
+		requestStackPush(States::ServerSelect);
+	});
+	m_buttonQuit.onClick([this]{
+		requestStackClear();
+		requestStackPush(States::Title);
+	});
 }
 
 void PauseState::renderTopScreen(cpp3ds::Window& window)
 {
-	window.draw(m_text);
+	window.draw(m_overlay);
 }
 
 void PauseState::renderBottomScreen(cpp3ds::Window& window)
 {
-	// Nothing
+	window.draw(m_overlay);
+
+	window.draw(m_buttonResume);
+	window.draw(m_buttonDisconnect);
+	window.draw(m_buttonQuit);
 }
 
 bool PauseState::update(float delta)
@@ -29,10 +60,15 @@ bool PauseState::update(float delta)
 
 bool PauseState::processEvent(const cpp3ds::Event& event)
 {
-	// If any key is pressed, trigger the next screen
+	m_buttonResume.processEvent(event);
+	m_buttonDisconnect.processEvent(event);
+	m_buttonQuit.processEvent(event);
+
 	if (event.type == cpp3ds::Event::KeyPressed)
 	{
-		requestStackPop();
+		if (event.key.code == cpp3ds::Keyboard::Start) {
+			requestStackPop();
+		}
 	}
 
 	// Don't propagate event processing
--- src/States/PauseState.hpp
+++ src/States/PauseState.hpp
@@ -2,24 +2,30 @@
 #define DRAWATTACK_PAUSESTATE_HPP
 
 #include "State.hpp"
+#include "../GUI/Button.hpp"
 #include <cpp3ds/Graphics/Sprite.hpp>
 #include <cpp3ds/Graphics/Text.hpp>
+#include <cpp3ds/Graphics/RectangleShape.hpp>
 
 namespace DrawAttack {
 
-	class PauseState : public State
-	{
-	public:
-		PauseState(StateStack& stack, Context& context);
+class PauseState : public State
+{
+public:
+	PauseState(StateStack& stack, Context& context);
 
-		virtual void renderTopScreen(cpp3ds::Window& window);
-		virtual void renderBottomScreen(cpp3ds::Window& window);
-		virtual bool update(float delta);
-		virtual bool processEvent(const cpp3ds::Event& event);
+	virtual void renderTopScreen(cpp3ds::Window& window);
+	virtual void renderBottomScreen(cpp3ds::Window& window);
+	virtual bool update(float delta);
+	virtual bool processEvent(const cpp3ds::Event& event);
 
-	private:
-		cpp3ds::Text m_text;
-	};
+private:
+	cpp3ds::Texture m_texture;
+	cpp3ds::RectangleShape m_overlay;
+	gui3ds::Button m_buttonResume;
+	gui3ds::Button m_buttonDisconnect;
+	gui3ds::Button m_buttonQuit;
+};
 
 } // namespace DrawAttack
 
